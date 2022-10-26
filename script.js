11f8574973aef4bb449c07d81a910c525c8dac96
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2022-10-25 22:37:41 -0500

    click search history buttons to populate their weather

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var submitButtonEl = $('#search-btn');
 
-// create function to get coordinates based on city input. pass latitude and longitude into getFiveDayForecast and getCurrentWeather functions
-var getCoordinates = function(event) {
+function handleSearch(event) {
     event.preventDefault();
-    var cityInputEl = $('#city-input');
-    console.log(cityInputEl);
-    var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityInputEl.val()}&appid=4709f071f7e8e51ca2c428e9f96726ca`;
+    query = $('#city-input').val();
+    getCoordinates(query);
+}
+
+// create function to get coordinates based on city input. pass latitude and longitude into getFiveDayForecast and getCurrentWeather functions
+var getCoordinates = function (query) {
+  
+    var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${query}&appid=4709f071f7e8e51ca2c428e9f96726ca`;
     fetch(apiUrl)
         .then(function (response) {
             return response.json();
@@ -15,47 +19,48 @@ var getCoordinates = function(event) {
             getFiveDayForecast(data[0].lat, data[0].lon);
             getCurrentWeather(data[0].lat, data[0].lon);
             storeCity(data[0].lat, data[0].lon);
+         
         })
 }
 
 var getCurrentWeather = function (lat, lon) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=4709f071f7e8e51ca2c428e9f96726ca`;
     fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var currentCity = $('#current-city');
-        var currentDate = $('#current-date');
-        var currentIcon = $('#current-icon');
-        var currentTemp = $('#current-temp');
-        var currentWind = $('#current-wind');
-        var currentHumidity = $('#current-humidity');
-        currentCity.text(data.name);
-        currentDate.text(moment().format('MM/DD/YYYY'));
-        currentIcon.html(`<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`);
-        currentTemp.text('Temp: ' + data.main.temp + '°F');
-        currentWind.text('Wind: ' + data.wind.speed + ' MPH');
-        currentHumidity.text('Humidity: ' + data.main.humidity + '%');
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var currentCity = $('#current-city');
+            var currentDate = $('#current-date');
+            var currentIcon = $('#current-icon');
+            var currentTemp = $('#current-temp');
+            var currentWind = $('#current-wind');
+            var currentHumidity = $('#current-humidity');
+            currentCity.text(data.name);
+            currentDate.text(moment().format('MM/DD/YYYY'));
+            currentIcon.html(`<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png">`);
+            currentTemp.text('Temp: ' + data.main.temp + '°F');
+            currentWind.text('Wind: ' + data.wind.speed + ' MPH');
+            currentHumidity.text('Humidity: ' + data.main.humidity + '%');
+        })
 }
 
 var getFiveDayForecast = function (lat, lon) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=4709f071f7e8e51ca2c428e9f96726ca`;
     fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-        var fiveDayLabel = $('#five-day-label');
-        fiveDayLabel.text('5-Day Forecast:');
-        var fiveDayForecast = $('#five-day-forecast');
-        fiveDayForecast.empty();
-        $('#city-form')[0].reset();
-        for (var i = 0; i < data.list.length; i+=8) {
-            var html = ` 
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            var fiveDayLabel = $('#five-day-label');
+            fiveDayLabel.text('5-Day Forecast:');
+            var fiveDayForecast = $('#five-day-forecast');
+            fiveDayForecast.empty();
+            $('#city-form')[0].reset();
+            for (var i = 0; i < data.list.length; i += 8) {
+                var html = ` 
             <div class="card" style="width: 11rem; background-color: rgb(0, 157, 255); margin-bottom: 1em;">
             <div class="card-body">
             <h5 class="card-title" style="color:white;">${moment.unix(data.list[i].dt).format('MM/DD/YYYY')}</h5>
@@ -66,45 +71,68 @@ var getFiveDayForecast = function (lat, lon) {
             </div>
             </div>
             `
-            fiveDayForecast.append(html);
-        }
-        
-    })
+                fiveDayForecast.append(html);
+            }
+
+        })
 }
 
 var storeCity = function (lat, lon) {
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=4709f071f7e8e51ca2c428e9f96726ca`;
     fetch(apiUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
-        // create array to hold cities
-        var citiesArr= [];
-        var city = data.name;
-        // if the array does not include city, add it to the array
-        if (!citiesArr.includes(city)) {
-            citiesArr.push(city);
-        }
-        console.log(citiesArr);
-        // add cities array to local storage
-        var storeCities = localStorage.setItem('city', JSON.stringify(citiesArr));
-        // get cities array from local storage
-        var getStoredCity = localStorage.getItem('city', JSON.parse(storeCities));
-        // reassign cities array to stored array
-        citiesArr = getStoredCity;
-        // add each item in array to button and div
-        for (var i = 0; i < citiesArr.length; i++) {
-            var storedCityEl = $('<button class="btn btn-primary">');
-            var storedCity = storedCityEl.text(getStoredCity[i]);
-            $('#search-history').append(storedCity);
-        }
-    })
+            var city = data.name;
+            var getStoredCities = getCities();
+            if (getStoredCities === null) {
+                var citiesArr = [];
+            } else {
+                var citiesArr = getStoredCities;
+            }
+            // if the array does not include city, add it to the array
+            console.log(!citiesArr.includes(city));
+            if (!citiesArr.includes(city)) {
+                citiesArr.push(city);
+            }
+            localStorage.setItem('city', JSON.stringify(citiesArr));
+            console.log(citiesArr);
+        init();
+        })
+
 }
 
-submitButtonEl.on('click', getCoordinates);
+function getCities() {
+    return JSON.parse(localStorage.getItem('city'));
+}
 
+// create global createButton function, argument will be city
+function init() {
+    $('#search-history').html('');
+    var citiesArr = getCities();
+    console.log(citiesArr);
+    if (citiesArr) {
+        console.log("success");
+        for (var i = 0; i < citiesArr.length; i++) {
+            var storedCityEl = $('<button class="btn btn-primary">');
+            var storedCity = storedCityEl.text(citiesArr[i]);
+            $('#search-history').append(storedCity);
+    }
+
+    }
+}
+init();
+
+submitButtonEl.on('click', handleSearch);
+$('#search-history').on('click', reSearch);
+
+function reSearch(event) {
+    getCoordinates(event.target.innerText)
+
+}
+// use event bubbling, and event.Target.innerText on search history div to make search history buttons clickable
 // city repeats appear in div
 // put array into local storage
